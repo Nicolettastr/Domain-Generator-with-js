02547979213a60e6b86ba0f2fc5635f83c86a817
Nicolettastr <nicolettastruggia@hotmail.com>
2022-11-18 15:34:53 +0000

    Adding files of excersice domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const domain = document.querySelector(".alert");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "that", "a"];
+  let adj = ["great", "big", "little", "gorgeous"];
+  let noun = ["jogger", "racoon", "dog", "rigo"];
+  let extensions = ["com", "es", "us", "io"];
+
+  function loopArray() {
+    pronoun.forEach(element => {
+      adj.forEach(item => {
+        noun.forEach(object => {
+          extensions.forEach(ext => {
+            domain.innerHTML += `<p> ${element}${item}${object}.${ext} </p>`;
+          });
+        });
+      });
+    });
+  }
+
+  loopArray();
 };
